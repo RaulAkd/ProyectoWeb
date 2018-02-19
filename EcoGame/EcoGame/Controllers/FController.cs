ba1c0e48e923c61fad427a8cd132b7c90a5eece8
LAPMABE\Maria Belen <mabegc94@gmail.com>
2018-02-18 23:59:24 -0500

    Panel de juego

---
+++ EcoGame/EcoGame/Controllers/FController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcoGame.Controllers
+{
+    public class FController : Controller
+    {
+        // GET: F
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
