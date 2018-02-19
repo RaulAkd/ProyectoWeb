c57eadc331be923bb144ac66168ee3873f5bbd26
LAPMABE\Maria Belen <mabegc94@gmail.com>
2018-02-19 04:07:28 -0500

    Index

---
+++ EcoGame/EcoGame/Controllers/InformaciónAnimalesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcoGame.Controllers
+{
+    public class InformaciónAnimalesController : Controller
+    {
+        // GET: InformaciónAnimales
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: InformaciónAnimales
+        public ActionResult IndexCondor()
+        {
+            return View();
+        }
+
+        // GET: InformaciónAnimales
+        public ActionResult IndexCarpintero()
+        {
+            return View();
+        }
+        // GET: InformaciónAnimales
+        public ActionResult IndexPuma()
+        {
+            return View();
+        }
+        // GET: InformaciónAnimales
+        public ActionResult IndexPinguino()
+        {
+            return View();
+        }
+        // GET: InformaciónAnimales
+        public ActionResult IndexOso()
+        {
+            return View();
+        }
+        // GET: InformaciónAnimales
+        public ActionResult IndexEspin()
+        {
+            return View();
+        }
+    }
+}
